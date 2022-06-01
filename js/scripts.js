c8452cf2dc8564d9e23327302e0d8d07cf3ac3a1
ftolentino <filmer.tolentino@gmail.com>
2022-05-31 22:38:07 -0700

    CHANGE const result to manually equal subtract, multiply, and divide functions

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,6 @@ function divide(number1, number2) {
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = subtract(number1, number2);
+const result = divide(number1, number2);
 
 alert(result);
